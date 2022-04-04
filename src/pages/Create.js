1dd6a8d3b2caaa93bcd113b2d6ebff57aeb62ed7
jaden-wei <jaden.wei2003@gmail.com>
2022-04-04 02:06:34 -0400

    create working

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { addDoc, collection } from "firebase/firestore";
+import { db, auth } from "../Firebase";
+
+import { useNavigate } from "react-router-dom";
 
 function Create() {
-  return <div>Create</div>;
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
+
+  let nav = useNavigate();
+
+  const postsCollectionRef = collection(db, "posts");
+
+  const createPost = async () => {
+    await addDoc(postsCollectionRef, {
+      title,
+      text,
+      author: { name: auth.currentUser.displayName, id: auth.currentUser.uid },
+    });
+    nav("/");
+  };
+
+  return (
+    <div>
+      <div>
+        <h1>Create Post</h1>
+        <div>
+          <input
+            placeholder="Title"
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <textarea
+            placeholder="Type here..."
+            onChange={(e) => {
+              setText(e.target.value);
+            }}
+          />
+        </div>
+        <button onClick={createPost}>Submit Post</button>
+      </div>
+    </div>
+  );
 }
 
 export default Create;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../Firebase";
 
 function Home() {
-  return <div>Home</div>;
+  const [posts, setPosts] = useState([]);
+
+  const postsCollectionRef = collection(db, "posts");
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postsCollectionRef);
+      setPosts(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getPosts();
+  });
+  return (
+    <div>
+      {posts.map((post) => {
+        return (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.text}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Home;
